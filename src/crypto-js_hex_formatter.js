5e4ef5a4637522cb626f0b6a0b875b1be7686322
Gabor Heja <gheja@cubeone>
2012-12-19 23:35:46 +0100

    Moved CryptoJS Hex formatter to a separate crypto-js_hex_formatter.js script (no more patching needed), added param_password_decode to decryption password

---
+++ src/crypto-js_hex_formatter.js
@@ -0,0 +1,49 @@
+/* extensions to CryptoJS */
+CryptoJS.lib.Cipher.HexFormatter || (function (undefined) {
+    // Shortcuts
+    
+    // Shortcuts
+    var C = CryptoJS;
+    var C_enc = C.enc;
+    var C_lib = C.lib;
+    var C_format = C.format;
+    var Hex = C_enc.Hex;
+    var CipherParams = C_lib.CipherParams;
+    
+    var HexFormatter = C_format.Hex = {
+        /**
+         * Converts a cipher params object to a hexadecimally encoded string.
+         *
+         * @param {CipherParams} cipherParams The cipher params object.
+         *
+         * @return {string} The OpenSSL-compatible string.
+         *
+         * @static
+         *
+         * @example
+         *
+         *     var hexString = CryptoJS.format.Hex.stringify(cipherParams);
+         */
+        stringify: function (cipherParams) {
+            return cipherParams.ciphertext.toString(Hex);
+        },
+
+        /**
+         * Converts a hexadecimally encoded string to a cipher params object.
+         *
+         * @param {string} input The hexadecimally encoded string.
+         *
+         * @return {CipherParams} The cipher params object.
+         *
+         * @static
+         *
+         * @example
+         *
+         *     var cipherParams = CryptoJS.format.Hex.parse(hexString);
+         */
+        parse: function (input) {
+            var ciphertext = Hex.parse(input);
+            return CipherParams.create({ ciphertext: ciphertext });
+        }
+    };
+}());
