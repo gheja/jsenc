5e20dfda2a4fe3f42b21518bfb482354029be1e6
Gabor Heja <gheja@ubuntu2>
2012-12-27 00:07:33 +0000

    jsenc02.js: removed some unused code

--- src/jsenc02.js
+++ src/jsenc02.js
@@ -14,49 +14,9 @@ function _update_container_data()
 	{
 		document.getElementById("container_current_status").value = "Container is not open.";
 	}
-// 	document.getElementById("master_key_cipher").value = _container.masterKey.cipher;
 	document.getElementById("master_key_key").value = _container.masterKey.key;
 	document.getElementById("master_key_iv").value = _container.masterKey.iv;
 	
-//	document.getElementById("container_current_data").value = JSON.stringify(_container);
-/*
-	var i;
-	var s = "{\n";
-	s += "\tkeys: [\n";
-	if (_container.keys.length == 0)
-	{
-		s += "\t\t// no keys defined yet,\n";
-		s += "\t\t// this is an invalid state.\n";
-	}
-	else
-	{
-		for (i=0; i<_container.keys.length; i++)
-		{
-			if (_container.keys[i] == null)
-			{
-				s += "\t\t" + i + ": {},\n";
-			}
-			else
-			{
-				s += "\t\t" + i + ": { \n";
-				s += "\t\t\t\"encoding\": " + _container.keys[i].encoding + ",\n";
-				s += "\t\t\t\"algo\": " + _container.keys[i].algo + ",\n";
-				s += "\t\t\t\"salt\": " + _container.keys[i].salt + ",\n";
-				s += "\t\t\t\"iterations\": " + _container.keys[i].iterations + ",\n";
-				s += "\t\t\t\"cipher\": " + _container.keys[i].cipher + ",\n";
-				s += "\t\t\t\"iv\": " + _container.keys[i].iv + ",\n";
-				s += "\t\t\t\"master_key_data\": " + _container.keys[i].master_key_data + "\n";
-				s += "\t\t},\n";
-			}
-		}
-		s = s.substring(0, s.length - 2) + "\n";
-	}
-	s += "\t],\n";
-	s += "\t\"encoding\": \"hex\",\n";
-	s += "\t\"data\": \"" + _container.data + "\"\n";
-	s += "}\n";
-	document.getElementById("container_current_data").value = s;
-*/
 	document.getElementById("container_current_data").value = _container.getJson();
 }
 
